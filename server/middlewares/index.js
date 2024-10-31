db35497178b693037d0b9660240240c67372538b
Mihail Tomov <62585647+mihailtomov@users.noreply.github.com>
2024-10-31 21:05:50 +0200

    modify allow origin header to allow cors

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -10,7 +10,7 @@ await client.connect();
 
 const serverHeaders = (req, res, next) => {
   res.set({
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET',
     // 'Cache-Control': 'max-age=604800',
   });
