18a7c2d1fbc826a8cb95945236be6e2b087b6c20
Mihail Tomov <62585647+mihailtomov@users.noreply.github.com>
2024-11-03 16:27:08 +0200

    fix cron time to avoid pinging every second at the specified minute

--- server/utils/cron-job.js
+++ server/utils/cron-job.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 // initialize cron job hitting bsl-stats home endpoint every 14 minutes
 export const initializeCronJob = () =>
   new CronJob(
-    '* */14 * * * *', // Every 14 minutes starting at :00 minute after the hour
+    '0 */14 * * * *', // At second :00, every 14 minutes starting at minute :00, of every hour
     () => fetch('https://bsl-stats-server.onrender.com/'),
     null, // onComplete
     true, // start
