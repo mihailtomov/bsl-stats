8deeb056e07d4eebe538802ada8ad5e6101f9459
Mihail Tomov <62585647+mihailtomov@users.noreply.github.com>
2024-11-03 18:05:06 +0200

    fetch resources from deployed server in production

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,5 +1,11 @@
+const baseUrl = 'https://bsl-stats-server.onrender.com';
+
 export const fetchJsonData = async (url: string) => {
-  const response = await fetch(url);
+  const fetchUrl =
+    url.startsWith('/') && process.env.NODE_ENV === 'production'
+      ? baseUrl + url
+      : url;
+  const response = await fetch(fetchUrl);
   const data = await response.json();
 
   return data;
