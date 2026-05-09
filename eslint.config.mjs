50a449e15d5c0450388a8fcd69d6ef986bee0624
Mihail Tomov <62585647+mihailtomov@users.noreply.github.com>
2026-05-09 11:35:47 +0300

    fix eslint and ts errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,20 +9,20 @@ import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
 
 import packageJson from './package.json' with { type: 'json' };
 
-export default defineConfig({
-  files: ['**/*.{ts,tsx}'],
-  plugins: { 'simple-import-sort': simpleImportSortPlugin },
-  rules: {
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    'comma-spacing': ['error', { before: false, after: true }]
-  },
-  extends: [
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-    reactPlugin.configs.flat.recommended,
-    reactHooksPlugin.configs.flat.recommended
-  ],
-  // Base the version of react on the dependencies
-  settings: { react: { version: packageJson.dependencies.react } }
-});
+export default defineConfig([
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactHooksPlugin.configs.flat.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: { 'simple-import-sort': simpleImportSortPlugin },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'comma-spacing': ['error', { before: false, after: true }]
+    },
+    // Base the version of react on the dependencies
+    settings: { react: { version: packageJson.dependencies.react } }
+  }
+]);
